8298fe761894a573f4bdcdd023f2bf0d73b3eb39
RogelGS <rogel.gnz.snz@gmail.com>
2021-07-13 15:24:33 -0500

    Practica 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, useState } from "react";
+import BarraTitulo from './Componentes/BarraTitulo';
+import Highcharts from 'highcharts';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  useState = {
+    registros: [1, 2, 3, 4, 5, 6, 7].map(day => [
+      +moment().add(day, "days"), 
+      Math.random() * 200
+    ])
+  }
+
+  componentDidMount() {
+    this.graficar();
+  }
+
+  graficar = () => {
+    Highcharts.chart('grafico', {
+      title: {
+        text: "Mi Registro de peso"
+      },
+      xAxis: {
+        type: "datetime"
+      },
+      series: [
+        {
+          name: "test",
+          data: this.useState.registros
+        }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <BarraTitulo></BarraTitulo>
+        <main>
+          <div className="valing-wrapper">
+            <h2>Registro Diario de peso</h2>
+          </div>
+          <div className="row">
+            <div className="col s6">
+              <div id="grafico" className="z-depth-2 hoverable"></div>
+            </div>
+            <div className="col s6">
+              <table className="z-depth-2 hoverable">
+                <thead>
+                  <tr>
+                    <th>Fecha</th>
+                    <th>Peso (Libras)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{new Date().toLocaleDateString()}</td>
+                    <td>208 lb</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </main>
+        <button className="btn">Click me</button>
+        Hola mundo
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Componentes/BarraTitulo.js
@@ -0,0 +1,9 @@
+const BarraTitulo = () => (
+    <nav>
+        <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Mi Registro de Peso</a>
+        </div>
+    </nav>
+);
+
+export default BarraTitulo;
